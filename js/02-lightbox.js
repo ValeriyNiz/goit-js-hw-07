46be8b222cbc1f092a1ed060395bf1eb42433bee
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-08-12 22:19:23 +0300

    homework 7 task 2

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  const galleryItemsHtml = galleryItems.reduce((acc, { description, original, preview }) => {
+    const galleryItem = `      
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>      
+    `;
+
+    return acc + galleryItem;
+  }, '');
+
+  gallery.insertAdjacentHTML('beforeend', galleryItemsHtml);
+}
+
+renderGallery();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
